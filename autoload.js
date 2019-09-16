f8862d204aede34981f547df549d6f4bba2c9ec1
周勤 <2549821359@qq.com>
2019-09-16 13:42:27 +0800

    '[ADD]'

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 //注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/zzzz1997/live2d-widget/";
 //const live2d_path = "/live2d-widget/";
 
 //加载waifu.css
